ced96f81b5a1a0c6a583a335c963dd21b2c58197
Rukonju <rukonbds@gmail.com>
2021-08-20 20:59:43 +0600

    js added

---
+++ interaction.js
@@ -0,0 +1,3 @@
+document.getElementById("player-header").style.color="rgb(106, 90, 205)";
+document.getElementById("players").style.backgroundColor="rgb(251, 192, 0)"
+document.getElementById("blog-header").style.color="rgba(34,54,87,.9)"
